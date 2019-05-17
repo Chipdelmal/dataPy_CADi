6ee208be24d52b74b10e06aa8ffda0bbebe31290
chipdelmal <chipdelmal@gmail.com>
2019-05-17 08:54:21 -0700

    Plotting "housing" demo and calculating correlation.

--- scripts/housing.py
+++ scripts/housing.py
@@ -1,6 +1,8 @@
 ###############################################################################
 # "Housing data" example
 ###############################################################################
+#  Objectives:
+#
 #  Source:
 #   https://github.com/ageron/handson-ml/blob/master/02_end_to_end_machine_learning_project.ipynb
 ###############################################################################
@@ -12,25 +14,31 @@
 #   https://github.com/ageron/handson-ml/tree/master/datasets/housing
 # Pandas API:
 #   https://pandas.pydata.org/pandas-docs/stable/reference/api/
+# CMaps:
+#   https://matplotlib.org/tutorials/colors/colormaps.html
 # -----------------------------------------------------------------------------
 
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Import required libraries
 import matplotlib as plt
-import numpy
-import pandas
+import matplotlib.pyplot as plt
+import numpy as np
 import scipy
 import sklearn
 import pandas as pd
 #%matplotlib inline
 
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Setup data path
 DATA_PATH = "../data/extracted/housing/"
 DATA_FILE = "housing.csv"
 FULL_PATH = DATA_PATH + DATA_FILE
 
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Read the CSV file into a data frame
 data = pd.read_csv(FULL_PATH)
 
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Get information about our dataframe
 data.head(10)
 data.info()
@@ -40,8 +48,34 @@ print(headers)
 data["ocean_proximity"]
 data["ocean_proximity"].value_counts()
 
-
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Get some basic statistics and plots
 data.describe()
-data.hist(bins=50, figsize=(20,15))
+data.hist(bins=50, figsize=(20, 15))
 #plt.show()
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Create a test set
+def splitTrain(data, ratio):
+    ixShuffle = np.random.permutation(len(data))
+    testSetSize = int(len(data) * ratio)
+    ixTest = ixShuffle[:testSetSize]
+    ixTrain = ixShuffle[testSetSize:]
+    return (data.iloc[ixTrain], data.iloc[ixTest])
+
+(train,test) = splitTrain(data, .05)
+test
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Plotting
+data.plot(kind="scatter", x="longitude", y="latitude")
+data.plot(kind="scatter", x="longitude", y="latitude", alpha=.2)
+data.plot(
+    kind="scatter", x="longitude", y="latitude",
+    alpha=.15, s=data["population"]/100, figsize=(10,7), label="population",
+    c="median_house_value", cmap=plt.get_cmap("RdPu"), colorbar=True
+)
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Correlation (Pearson)
+data.corr()
