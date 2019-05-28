f5c6d36e6dd3b069b1c3dedcc51ee2515e2a95db
chipdelmal <chipdelmal@gmail.com>
2019-05-27 20:33:31 -0700

    Updating the CRISPR exercise.

--- scripts/trendsCRISPR.py
+++ scripts/trendsCRISPR.py
@@ -11,27 +11,39 @@
 # Import required libraries
 %matplotlib inline
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Loading the US trends for CRISPR
 data = pd.read_csv("../data/extracted/Trends/CRISPR.csv", header=1)
 data.keys()
 data.rename(columns={"Month": "month", 'crispr: (United States)': "US"},inplace=True)
+data["US"].unique()
+data.replace("<1",0, inplace=True)
+data["US"].unique()
+data["US"] = data["US"].astype(int)
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Loading the worldwide trends for CRISPR
 dataW = pd.read_csv("../data/extracted/Trends/CRISPRWorld.csv", header=1)
 dataW.keys()
-dataW.rename(columns={"Month": "month", 'CRISPR: (Worldwide)': "World"}, inplace=True)
-
+dataW.rename(columns={"Month": "month", 'crispr: (Worldwide)': "World"}, inplace=True)
+dataW.dtypes
+dataW["World"].unique()
+dataW.replace("<1",0, inplace=True)
+dataW["World"].unique()
+dataW["World"] = dataW["World"].astype(int)
 
 pd.concat([data, dataW], axis=1)
 dataFull = pd.merge(data, dataW, how="inner", on="month")
-dataFull
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Loading the Mexican trends for CRISPR with a reduced timespan
 dataM = pd.read_csv("../data/extracted/Trends/CRISPRMx.csv", header=1)
 dataM.keys()
 dataM.rename(columns={"Month": "month", 'crispr: (Mexico)': "Mx"}, inplace=True)
-pd.merge(dataM, dataFull, how="left", on="month")
+
+
+dataFull = pd.merge(dataM, dataFull, how="inner", on="month")
+dataFull.plot()
+plt.savefig('./images/crispr.png',dpi=500)
